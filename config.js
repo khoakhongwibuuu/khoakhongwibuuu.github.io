aae7108e442150527dd57ee3cebe17f8d0c1af08
ToiTenLaKhoa <anhkhoa.vv@gmail.com>
2023-07-01 15:48:05 +0700

    ⬆️ Updated

--- config.js
+++ config.js
@@ -2,13 +2,4 @@ const FOLLOWER_VAL = 3;
 const FOLLOWING_VAL = 22;
 const REPO_VAL = 10;
 const STAR_VAL = 13;
-
-let followers = document.querySelector("#followers-count");
-let followings = document.querySelector("#following-count");
-let repos = document.querySelector("#repo-count");
-let stars = document.querySelector("#star-count");
-
-followers.setAttribute("data-val", FOLLOWER_VAL)
-followings.setAttribute("data-val", FOLLOWING_VAL)
-repos.setAttribute("data-val", REPO_VAL)
-stars.setAttribute("data-val", STAR_VAL)
+const PROFILE_LINK = 'khoakhongwibuuu.github.io'
--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let valueDisplays = document.querySelectorAll(".num");
 let interval = 200;
 
+let followers = document.querySelector("#followers-count");
+let followings = document.querySelector("#following-count");
+let repos = document.querySelector("#repo-count");
+let stars = document.querySelector("#star-count");
+
+followers.setAttribute("data-val", FOLLOWER_VAL)
+followings.setAttribute("data-val", FOLLOWING_VAL)
+repos.setAttribute("data-val", REPO_VAL)
+stars.setAttribute("data-val", STAR_VAL)
+
 valueDisplays.forEach((valueDisplay) => {
     let startValue = -1;
     let endValue = parseInt(valueDisplay.getAttribute("data-val"));
@@ -15,7 +25,7 @@ valueDisplays.forEach((valueDisplay) => {
 });
 
 document.querySelector("#link-copy").addEventListener("click", function () {
-    navigator.clipboard.writeText("toitenlakhoa.github.io");
+    navigator.clipboard.writeText(PROFILE_LINK);
     document.querySelector('.js-profile-card').classList.add('active');
     setTimeout(function () {
         document.querySelector('.js-profile-card').classList.remove('active');
